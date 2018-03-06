a47ce21ac28240a804882f4d07a0ba225a7653e7
David Sally <davidcsally@gmail.com>
2018-03-05 22:45:05 -0800

    (add) ansi color support

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,43 @@
 'use strict';
 
 module.exports = {
-  default: '\x1b[0m',
-  yellow: '\x1b[33m',
-  bgMagenta: '\x1b[45m',
+  // non color attributes
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  underscore: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+
+  // foreground
+  FgBlack: '\x1b[30m',
+  FgRed: '\x1b[31m',
+  FgGreen: '\x1b[32m',
+  FgYellow: '\x1b[33m',
+  FgBlue: '\x1b[34m',
+  FgMagenta: '\x1b[35m',
+  FgCyan: '\x1b[36m',
+  FgWhite: '\x1b[37m',
+  FgDefault: '\x1b[39m',
+
+  // background
+  BgBlack: '\x1b[40m',
+  BgRed: '\x1b[41m',
+  BgGreen: '\x1b[42m',
+  BgYellow: '\x1b[43m',
+  BgBlue: '\x1b[44m',
+  BgMagenta: '\x1b[45m',
+  BgCyan: '\x1b[46m',
+  BgWhite: '\x1b[47m',
+  BgDefault: '\x1b[49m',
+
+  BgLighGray: '\x1b[100m',
+  BgLightRed: '\x1b[101m',
+  BgLightGreen: '\x1b[102m',
+  BgLightYellow: '\x1b[103m',
+  BgLightBlue: '\x1b[104m',
+  BgLightMagenta: '\x1b[105m',
+  BgLightCyan: '\x1b[106m',
+  BgLightWhite: '\x1b[107m',
 };
--- src/utils.js
+++ src/utils.js
@@ -2,22 +2,54 @@ const colors = require('./colors');
 
 module.exports = {
   setBarLength: (barLength) => barLength,
+
   setJoinChar: (char) => char,
+
   setProgChar: (char) => char,
-  setBgColor: (color) => {
-    switch (color) {
-      case 'magenta':
-        return colors.bgMagenta;
-      default:
-        return colors.default;
-    }
-  },
+
   setTextColor: (color) => {
     switch (color) {
+      case 'black':
+        return colors.FgBlack;
+      case 'red':
+        return colors.FgRed;
+      case 'green':
+        return colors.FgGreen;
       case 'yellow':
-        return colors.yellow;
+        return colors.FgYellow;
+      case 'blue':
+        return colors.FgBlue;
+      case 'magenta':
+        return colors.FgMagenta;
+      case 'cyan':
+        return colors.FgCyan;
+      case 'white':
+        return colors.FgWhite;
       default:
-        return colors.default;
+        return colors.FgDefault;
+    }
+  },
+
+  setBgColor: (color) => {
+    switch (color) {
+      case 'black':
+        return colors.BgBlack;
+      case 'red':
+        return colors.BgRed;
+      case 'green':
+        return colors.BgGreen;
+      case 'yellow':
+        return colors.BgYellow;
+      case 'blue':
+        return colors.BgBlue;
+      case 'magenta':
+        return colors.BgMagenta;
+      case 'cyan':
+        return colors.BgCyan;
+      case 'white':
+        return colors.BgWhite;
+      default:
+        return colors.BgDefault;
     }
   },
 };
